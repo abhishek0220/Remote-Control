5881035b5ccad45d0a6243ffd155c39f0e8a2c12
Sachin Kumar <sachindrck25@gmail.com>
2020-06-21 03:30:32 +0530

    developing webpage

--- controller/script.js
+++ controller/script.js
@@ -15,7 +15,6 @@ const createSocket = (clientId) => {
         if (message.event === 'disconnect') {
             document.getElementById('status').textContent = 'Disconnected'
             document.getElementById('value').textContent = ""
-            socket.close()
         }
         if (message.event === 'cpu') {
             document.getElementById('value').textContent = message.value
@@ -59,13 +58,4 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const lockscreen = document.getElementById('lock_screen')
     lockscreen.addEventListener('click', () => socket.send(JSON.stringify({ event: 'lockscreen' })))
-
-    const volp = document.getElementById('vol_plus')
-    volp.addEventListener('click', () => socket.send(JSON.stringify({ event: 'vol', type:'plus' })))
-
-    const volm = document.getElementById('vol_minus')
-    volm.addEventListener('click', () => socket.send(JSON.stringify({ event: 'vol', type:'minus' })))
-
-    const volmu = document.getElementById('vol_mu')
-    volmu.addEventListener('click', () => socket.send(JSON.stringify({ event: 'vol', type:'mu' })))
 })
